cb543f2b847ca06a76b07d2febf8763405ba9941
woniujun <54158801+sanbudewoniujun@users.noreply.github.com>
2022-04-28 23:14:30 +0800

    Update ksjsb_不看广告和直播.js

--- ksjsb_不看广告和直播.js
+++ ksjsb_不看广告和直播.js
@@ -28,7 +28,7 @@ ksapp/client/package/renew url script-request-header https://raw.githubuserconte
 hostname = api.kuaisho*.com
 hostname = open.kuaisho*.com
 */
-/*
+/* 
  * 加密工具已经升级了一个版本，目前为 jsjiami.com.v6 ，更新了加密算法，缩减了体积;
  * 另外 jsjiami.com.v6 已经强制加入校验，注释可以去掉，但是 jsjiami.com.v6 不能去掉，其他都没有任何绑定。
  * 誓死不会加入任何后门，JsJiami.com 加密的使命就是为了保护你们的Javascript 。
